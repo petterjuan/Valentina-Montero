f712b49f1cc700baff742260afa45c86de3c5f0b
Juan Petter (Petter) <petter2001us@gmail.com>
2025-09-29 18:08:09 +0000

    Te voy a dar un esqueleto completo de Pages Router para tu proyecto, lis

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import HeroSection from "@/components/sections/HeroSection";
+import LeadMagnetSection from "@/components/sections/LeadMagnetSection";
+import CoachingProgramsSection from "@/components/sections/CoachingProgramsSection";
+import MeetTheCoachSection from "@/components/sections/MeetTheCoachSection";
+import AiGeneratorSection from "@/components/sections/AiGeneratorSection";
+import TestimonialsSection from "@/components/sections/TestimonialsSection";
+import BlogSection from "@/components/sections/BlogSection";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <LeadMagnetSection />
+      <CoachingProgramsSection />
+      <MeetTheCoachSection />
+      <AiGeneratorSection />
+      <TestimonialsSection />
+      <BlogSection />
+    </>
+  );
+}
